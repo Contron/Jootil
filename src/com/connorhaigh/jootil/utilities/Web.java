b98e256a2ac511053fa25511c3203774078962ea
Contron <connorhaigh@gmail.com>
2014-05-24 10:43:10 +0100

    Added functionality to retrieve remote Web pages.

---
+++ src/com/connorhaigh/jootil/utilities/Web.java
@@ -0,0 +1,38 @@
+package com.connorhaigh.jootil.utilities;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Web 
+{
+	/**
+	 * Loads and retrieves the contents of a given web page.
+	 * @param url the URL to retrieve
+	 * @return the contents of the web page
+	 * @throws IOException if the web page could not be reached or read
+	 */
+	public static String retrieve(URL url) throws IOException
+	{
+		//create connection
+		HttpURLConnection httpUrlConnection = (HttpURLConnection) url.openConnection();
+		httpUrlConnection.setDoInput(true);
+		httpUrlConnection.setDoOutput(false);
+		httpUrlConnection.setUseCaches(false);
+		
+		try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpUrlConnection.getInputStream())))
+		{
+			//result
+			StringBuilder result = new StringBuilder();
+			
+			//read
+			String line = null;
+			while ((line = bufferedReader.readLine()) != null)
+				result.append(line + "\n");
+			
+			return result.toString();
+		}
+	}
+}

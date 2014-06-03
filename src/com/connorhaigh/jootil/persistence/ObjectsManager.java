bfe42e69624c1042d1a1d9c3ac372fb5d3d4c495
Contron <connorhaigh@gmail.com>
2014-06-03 20:23:07 +0100

    Added functionality to locally load/save objects from specific files.

--- src/com/connorhaigh/jootil/persistence/ObjectsManager.java
+++ src/com/connorhaigh/jootil/persistence/ObjectsManager.java
@@ -4,6 +4,7 @@ import java.beans.XMLDecoder;
 import java.beans.XMLEncoder;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
 
@@ -65,6 +66,41 @@ public class ObjectsManager<Element>
 		catch (Exception exception)
 		{
 			Jootil.LOGGER.severe("Could not save objects for " + this);
+		}
+	}
+	
+	/**
+	 * Loads a local object from file.
+	 * @param file the file to load
+	 * @return the object
+	 * @throws FileNotFoundException if the file could not be found
+	 */
+	@SuppressWarnings("unchecked")
+	public Element loadLocal(File file) throws FileNotFoundException
+	{
+		try (XMLDecoder xmlDecoder = new XMLDecoder(new FileInputStream(file)))
+		{
+			//read
+			xmlDecoder.setExceptionListener(exception -> Jootil.LOGGER.info("XML local object loading error: " + exception.getMessage()));
+			Element element = (Element) xmlDecoder.readObject();
+			
+			return element;
+		}
+	}
+	
+	/**
+	 * Saves a local object to file.
+	 * @param file the file to save
+	 * @param element the object to save
+	 * @throws FileNotFoundException if the file could not be found
+	 */
+	public void saveLocal(File file, Element element) throws FileNotFoundException
+	{
+		try (XMLEncoder xmlEncoder = new XMLEncoder(new FileOutputStream(file)))
+		{
+			//write
+			xmlEncoder.setExceptionListener(exception -> Jootil.LOGGER.info("XML local object saving error: " + exception.getMessage()));
+			xmlEncoder.writeObject(element);
 		}
 	}
 	

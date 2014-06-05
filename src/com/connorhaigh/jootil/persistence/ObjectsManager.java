fe2bc15de7e49edf6117511f796bb513cc270dbd
Contron <connorhaigh@gmail.com>
2014-06-05 22:21:54 +0100

    Moved local object methods to a separate class.

--- src/com/connorhaigh/jootil/persistence/ObjectsManager.java
+++ src/com/connorhaigh/jootil/persistence/ObjectsManager.java
@@ -59,38 +59,6 @@ public class ObjectsManager<Element>
 	}
 	
 	/**
-	 * Loads a local object from file.
-	 * @param file the file to load
-	 * @return the object
-	 * @throws FileNotFoundException if the file could not be found
-	 * @throws ClassCastException if the object is not an instance of this manager's class
-	 */
-	@SuppressWarnings("unchecked")
-	public Element loadLocal(File file) throws FileNotFoundException, ClassCastException
-	{
-		try (XMLDecoder xmlDecoder = new XMLDecoder(new FileInputStream(file)))
-		{
-			//read
-			return (Element) xmlDecoder.readObject();
-		}
-	}
-	
-	/**
-	 * Saves a local object to file.
-	 * @param file the file to save
-	 * @param element the object to save
-	 * @throws FileNotFoundException if the file could not be found
-	 */
-	public void saveLocal(File file, Element element) throws FileNotFoundException
-	{
-		try (XMLEncoder xmlEncoder = new XMLEncoder(new FileOutputStream(file)))
-		{
-			//write
-			xmlEncoder.writeObject(element);
-		}
-	}
-	
-	/**
 	 * Sets the object group for this manager.
 	 * @param objectGroup the object group
 	 */
--- src/com/connorhaigh/jootil/utilities/Styles.java
+++ src/com/connorhaigh/jootil/utilities/Styles.java
@@ -9,19 +9,21 @@ public class Styles
 	 */
 	public static String build(String... rules)
 	{
-		return String.join(" ", rules);
+		return (String.join(Styles.DELIMITER + " ", rules) + Styles.DELIMITER);
 	}
 	
-	public static final String CONTROL_BACKGROUND_COLOUR = "-fx-background-color: white;";
-	public static final String CONTROL_TRANSPARENT_BACKGROUND_COLOUR = "-fx-background-color: transparent;";
+	public static final String DELIMITER = ";";
 	
-	public static final String CONTROL_BORDER_COLOUR = "-fx-border-color: gray;";
+	public static final String CONTROL_BACKGROUND_COLOUR = "-fx-background-color: white";
+	public static final String CONTROL_TRANSPARENT_BACKGROUND_COLOUR = "-fx-background-color: transparent";
 	
-	public static final String CONTROL_ALL_BORDER = "-fx-border-width: 1 1 1 1;";
-	public static final String CONTROL_TOP_BORDER = "-fx-border-width: 1 0 0 0;";
-	public static final String CONTROL_LEFT_BORDER = "-fx-border-width: 0 1 0 0;";
-	public static final String CONTROL_BOTTOM_BORDER = "-fx-border-width: 0 0 1 0;";
-	public static final String CONTROL_RIGHT_BORDER = "-fx-border-width: 0 0 0 1;";
-	public static final String CONTROL_TOP_BOTTOM_BORDER = "-fx-border-width: 1 0 1 0;";
-	public static final String CONTROL_LEFT_RIGHT_BORDER = "-fx-border-width: 0 1 0 1;";
+	public static final String CONTROL_BORDER_COLOUR = "-fx-border-color: gray";
+	
+	public static final String CONTROL_ALL_BORDER = "-fx-border-width: 1 1 1 1";
+	public static final String CONTROL_TOP_BORDER = "-fx-border-width: 1 0 0 0";
+	public static final String CONTROL_LEFT_BORDER = "-fx-border-width: 0 1 0 0";
+	public static final String CONTROL_BOTTOM_BORDER = "-fx-border-width: 0 0 1 0";
+	public static final String CONTROL_RIGHT_BORDER = "-fx-border-width: 0 0 0 1";
+	public static final String CONTROL_TOP_BOTTOM_BORDER = "-fx-border-width: 1 0 1 0";
+	public static final String CONTROL_LEFT_RIGHT_BORDER = "-fx-border-width: 0 1 0 1";
 }

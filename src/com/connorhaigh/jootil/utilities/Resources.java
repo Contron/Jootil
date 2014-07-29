375a75a25ccb8da27c662a01110f5990800ee108
Contron <connorhaigh@gmail.com>
2014-07-29 20:52:14 +0100

    Added final modifiers.

--- src/com/connorhaigh/jootil/utilities/Resources.java
+++ src/com/connorhaigh/jootil/utilities/Resources.java
@@ -4,6 +4,6 @@ import javafx.scene.text.Font;
 
 public class Resources 
 {
-	public static Font MONOSPACED_FONT = Font.font("Monospaced");
-	public static Font LARGE_FONT = Font.font(18);
+	public static final Font MONOSPACED_FONT = Font.font("Monospaced");
+	public static final Font LARGE_FONT = Font.font(18);
 }

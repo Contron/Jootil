1aec18a661e09a5b6ee1eb390c08d16c4cae889c
Contron <connorhaigh@gmail.com>
2014-09-09 15:17:03 +0100

    Added a few more useful fonts.

--- src/com/connorhaigh/jootil/utilities/Fonts.java
+++ src/com/connorhaigh/jootil/utilities/Fonts.java
@@ -1,9 +1,14 @@
 package com.connorhaigh.jootil.utilities;
 
 import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 
 public class Fonts 
 {
 	public static final Font MONOSPACED_FONT = Font.font("Monospaced");
+	
 	public static final Font LARGE_FONT = Font.font(18);
+	public static final Font BOLD_FONT = Font.font(Font.getDefault().getName(), FontWeight.BOLD, Font.getDefault().getSize());
+	public static final Font ITALIC_FONT = Font.font(Font.getDefault().getName(), FontPosture.ITALIC, Font.getDefault().getSize());
 }

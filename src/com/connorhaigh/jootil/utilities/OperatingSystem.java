ac731a28a603cd56c3e9d5828e077993566eca6d
Contron <connorhaigh@gmail.com>
2014-10-01 21:59:37 +0100

    Small readability improvement.

--- src/com/connorhaigh/jootil/utilities/OperatingSystem.java
+++ src/com/connorhaigh/jootil/utilities/OperatingSystem.java
@@ -14,13 +14,21 @@ public class OperatingSystem
 		String osName = System.getProperty("os.name").toLowerCase();
 		
 		if (osName.contains("windows"))
+		{
 			return Platform.WINDOWS;
+		}
 		else if (osName.contains("mac"))
+		{
 			return Platform.MACINTOSH;
+		}
 		else if (osName.contains("linux") || osName.contains("unix"))
+		{
 			return Platform.LINUX;
+		}
 		else
+		{
 			return Platform.UNKNOWN;
+		}
 	}
 	
 	/**
@@ -48,5 +56,14 @@ public class OperatingSystem
 	public static boolean isLinux()
 	{
 		return (OperatingSystem.getPlatform() == Platform.LINUX);
+	}
+	
+	/**
+	 * Returns if the current operating system is unknown.
+	 * @return if the current operating system is unknown
+	 */
+	public static boolean isUnknown()
+	{
+		return (OperatingSystem.getPlatform() == Platform.UNKNOWN);
 	}
 }
